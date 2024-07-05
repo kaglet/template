7b5384e3c28b5f730be65415919ca0861e5d2d08
Kago <kmotlhabane769@gmail.com>
2024-07-05 17:08:50 +0200

    Ensure webpack files are in ES6 module format not CommonJS format

--- webpack.common.js
+++ webpack.common.js
@@ -1,62 +1,62 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-	mode: "development",
+export default {
+	mode: 'development',
 	entry: {
-		app: "./src/index.js",
+		app: './src/index.js',
 	},
 	plugins: [
 		new MiniCssExtractPlugin(),
 		new HTMLWebpackPlugin({
-			template: "./src/index.html",
-			inject: "body",
+			template: './src/index.html',
+			inject: 'body',
 		}),
 	],
 	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
+		filename: '[name].bundle.js',
+		path: path.resolve(process.cwd(), 'dist'),
 		clean: true,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
+				type: 'asset/resource',
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
+				type: 'asset/resource',
 			},
 			{
 				test: /\.(csv|tsv)$/i,
-				use: ["csv-loader"],
+				use: ['csv-loader'],
 			},
 			{
 				test: /\.xml$/i,
-				use: ["xml-loader"],
+				use: ['xml-loader'],
 			},
 		],
 	},
 	optimization: {
 		minimize: true,
 		minimizer: [
-			// For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+			// For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
 			// `...`,
 			new CssMinimizerPlugin(),
 		],
 	},
 	resolve: {
 		alias: {
-			Fonts: path.resolve(__dirname, "src/assets/fonts/"),
-			Images: path.resolve(__dirname, "src/assets/images/"),
-			Svg: path.resolve(__dirname, "src/assets/svgs/"),
+			Fonts: path.resolve(process.cwd(), 'src/assets/fonts/'),
+			Images: path.resolve(process.cwd(), 'src/assets/images/'),
+			Svg: path.resolve(process.cwd(), 'src/assets/svgs/'),
 		},
 	},
 };
