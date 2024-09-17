75022eba36a52f4b6e36424c71c0e8040798695a
Kago <kmotlhabane769@gmail.com>
2024-09-17 13:54:41 +0200

    Add jest testing with CommonJS syntax and babel support

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-// Template code is for testing and example import purposes
-
-import "./styles.css";
-import exampleImage from "Images/ahtziri-lagarde-4_FsMDmCc6A-unsplash.jpg";
-
 function component() {
-  const element = document.createElement("div");
-
-  element.classList.add("test");
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = exampleImage;
-
-  element.append("Hi", myIcon);
+  const element = 3;
 
   return element;
 }
 
-document.body.appendChild(component());
+module.exports = component;
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+const component = require('./index');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(component()).toBe(3);
+});
