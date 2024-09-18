f534444f10f23c0d597b0a467f0be83f08372160
Kago <kmotlhabane769@gmail.com>
2024-09-18 13:43:59 +0200

    Delete unneeded tests

--- src/capitalize.js
+++
@@ -1,9 +0,0 @@
-
-function capitalize(string) {
-    let char = string.charAt(0);
-    let upper = char.toUpperCase();
-    let newWord = upper + string.substring(1);
-    return newWord;
-}
-
-module.exports = capitalize;
