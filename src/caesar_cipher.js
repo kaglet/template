7285ad72b0487272c2dbed0f5f947db87d510ab0
Kago <kmotlhabane769@gmail.com>
2024-09-18 10:08:37 +0200

    Add tested caesarCipher function

--- src/caesar_cipher.js
+++ src/caesar_cipher.js
@@ -1,19 +1,34 @@
-// TODO: modulus from character position should reset so 26 + 1 = 0. 
+function shiftByCase(asciiVal, shift, lb, ub) {
+    let newShiftedResult = asciiVal + shift;
+    // if it shifts + 1 from "z" then from lowerbound it should go to "a" not 97 + 1 which is b
+    // this offset does that
+    let lowerbound = lb - 1; 
+    let upperbound = ub;
+
+    if (newShiftedResult > upperbound) {
+        let wrappedShift = lowerbound + newShiftedResult % upperbound; 
+        newShiftedResult = wrappedShift;
+    }
+
+    let shiftedChar = String.fromCharCode(newShiftedResult);
+
+    return shiftedChar;
+}
 
 function caesarCipher(string, shift) {
     // a = 97, z = 122, A = 65, Z = 90
-    console.log("a".charCodeAt(0), "z".charCodeAt(0), "A".charCodeAt(0), "Z".charCodeAt(0));
     let arr = string.split('');
     let newStr = "";
+
     for (let i = 0; i < arr.length; i++) {
-        let asciiVal = arr[i].charCodeAt(i);
+        let asciiVal = arr[i].charCodeAt(0);
         let isLetter = (asciiVal <= 122 && asciiVal >= 97) || (asciiVal <= 90 && asciiVal >= 65);
         if (isLetter) {
             let isUppercase = asciiVal <= 90 && asciiVal >= 65;
             if (isUppercase) {
-                newStr += String.fromCharCode(asciiVal + shift);
+                newStr += shiftByCase(asciiVal, shift, 65, 90);
             } else {
-                newStr += String.fromCharCode(asciiVal + shift);
+                newStr += shiftByCase(asciiVal, shift, 97, 122);
             }
         } else {
             newStr += arr[i];
